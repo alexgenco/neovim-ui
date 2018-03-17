5a5622541d14c8f8251b200549ce0d9b26388ca3
Alex Genco <alexgenco@gmail.com>
2018-03-17 11:33:40 -0700

    initial commit

---
+++ lib/neovim/ui.rb
@@ -0,0 +1,7 @@
+require "neovim/ui/version"
+
+module Neovim
+  module UI
+    # Your code goes here...
+  end
+end
---
+++ spec/neovim/ui_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Neovim::UI do
+  it "has a version number" do
+    expect(Neovim::UI::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
