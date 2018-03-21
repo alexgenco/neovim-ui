e833f5d3b1d9c47d1ea31278fff955fa185b53b3
Alex Genco <alexgenco@gmail.com>
2018-03-20 17:49:08 -0700

    Put Event in proper namespace

--- lib/neovim/ui/event.rb
+++ lib/neovim/ui/event.rb
@@ -1,25 +1,27 @@
 module Neovim
-  module Event
-    def self.redraw(message)
-      Redraw.new(message)
-    end
+  class UI
+    module Event
+      def self.redraw(message)
+        Redraw.new(message)
+      end
 
-    def self.input(key)
-      Input.new(key)
-    end
+      def self.input(key)
+        Input.new(key)
+      end
 
-    Redraw = Struct.new(:message) do
-      def received(handlers, _)
-        handlers[message.method_name.to_sym].each do |handler|
-          handler.call(message)
+      Redraw = Struct.new(:message) do
+        def received(handlers, _)
+          handlers[message.method_name.to_sym].each do |handler|
+            handler.call(message)
+          end
         end
       end
-    end
 
-    Input = Struct.new(:key) do
-      def received(handlers, session)
-        handlers[:input].each { |handler| handler.call(key) }
-        session.notify(:nvim_input, key)
+      Input = Struct.new(:key) do
+        def received(handlers, session)
+          handlers[:input].each { |handler| handler.call(key) }
+          session.notify(:nvim_input, key)
+        end
       end
     end
   end
